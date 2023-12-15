e918f3a9dee77bbc8bb82ad3cea5421021a13f00
Rens Oliemans <hallo@rensoliemans.nl>
2023-12-15 12:11:37 +0100

    Cleanup day14 a bit

--- 2023/python/day14/henk.py
+++ 2023/python/day14/henk.py
@@ -23,6 +23,31 @@ def main(inputfile, part):
         return part_two(platform)
 
 
+def part_one(platform):
+    platform = roll(platform, Direction.NORTH)
+
+    platform = remove_pad(platform)
+    return north_load(platform)
+
+
+def part_two(platform, cycles=1000000000, recurse=True):
+    previous = list()
+    i = 0
+    rolls = cycles * 4
+    for i in range(rolls):
+        direction = Direction(i % 4)
+        platform = roll(platform, direction)
+        if recurse and platform in previous and direction == Direction.EAST:
+            index = previous.index(platform)
+            remainder = (rolls - i - 1) % (i - index)
+            return part_two(platform, remainder // 4, False)
+
+        previous.append(deepcopy(platform))
+
+    platform = remove_pad(platform)
+    return north_load(platform)
+
+
 def pad(platform):
     l, w = len(platform), len(platform[0])
     for r in platform:
@@ -46,9 +71,17 @@ def remove_pad(platform):
     return platform
 
 
+def north_load(platform):
+    total = 0
+    for i, line in enumerate(platform):
+        #print(f"{''.join(line)} {str(len(platform) - i).rjust(2, ' ')}")
+        total += len([r for r in line if r == 'O']) * (len(platform) - i)
+
+    return total
+
+
 def roll(platform, direction):
-    # We're mutating platform which isn't really nice, but since we are returning
-    # it anyway and not using the original value I'm OK with it.
+    # All roll methods update the platform list in-place.
     match direction:
         case direction.NORTH:
             roll_north(platform)
@@ -62,6 +95,7 @@ def roll(platform, direction):
     return platform
 
 
+# Warning: fantastic solution below
 def roll_north(platform):
     for i, row in enumerate(platform):
         for j, char in enumerate(platform[i]):
@@ -118,42 +152,6 @@ def roll_south(platform):
                 if platform[i + adjacent][j] == '.':
                     platform[i + adjacent][j] = 'O'
                     platform[i][j] = '.'
-
-
-def part_one(platform):
-    platform = roll(platform, Direction.NORTH)
-
-    platform = remove_pad(platform)
-
-    return load(platform)
-
-
-def load(platform):
-    total = 0
-    for i, line in enumerate(platform):
-        #print(f"{''.join(line)} {str(len(platform) - i).rjust(2, ' ')}")
-        total += len([r for r in line if r == 'O']) * (len(platform) - i)
-
-    return total
-
-
-def part_two(platform, cycles=1000000000, recurse=True):
-    previous = list()
-    i = 0
-    rolls = cycles * 4
-    for i in range(rolls):
-        direction = Direction(i % 4)
-        platform = roll(platform, direction)
-        if recurse and platform in previous and direction == Direction.EAST:
-            index = previous.index(platform)
-            remainder = (rolls - i - 1) % (i - index)
-            return part_two(platform, remainder // 4, False)
-
-        previous.append(deepcopy(platform))
-
-
-    platform = remove_pad(platform)
-    return load(platform)
 
 
 def pretty_print(platform):
