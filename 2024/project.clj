7ddfacaad44f4eaa00e3738e76bc7e16adb2930e
Rens Oliemans <hallo@rensoliemans.nl>
2024-12-10 20:58:19 +0100

    Finish day8

---
+++ 2024/project.clj
@@ -0,0 +1,7 @@
+(defproject aoc "0.1"
+  :description "Advent of Code by Rens"
+  :license "GPL v3.0"
+  :url "https://git.sr.ht:~rensoliemans/aoc"
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/math.combinatorics "0.3.0"]]
+  :plugins [[lein-tar "3.2.0"]])
