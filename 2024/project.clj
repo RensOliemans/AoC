57fd52837ef8dcb2a64cf8500cd36d15680ee3a5
Rens Oliemans <hallo@rensoliemans.nl>
2024-12-11 15:20:26 +0100

    Revisit some days
    - Use aoc.util/grid functions for Days 4 and 6.
      That explains the reordering of the cardinal directions: day 6
      requires the directions to be traversed in clockwise direction.
    - Add Optimizations headings in Day 5.
    - Add running time everywhere.
    - Cache some more.
    - Rewrite some paragraphs so they flow better, or so that some days
      fit neater on a single page.
    - Clean some code.
    - Add criterium dependency
      This allows me to quickly benchmark a function in the repl.

--- 2024/project.clj
+++ 2024/project.clj
@@ -3,5 +3,6 @@
   :license "GPL v3.0"
   :url "https://git.sr.ht:~rensoliemans/aoc"
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/math.combinatorics "0.3.0"]]
+                 [org.clojure/math.combinatorics "0.3.0"]
+                 [criterium "0.4.6"]]
   :plugins [[lein-tar "3.2.0"]])
