f3e1b550f11fd11967ca946bac96c5145ba4c590
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-09 20:39:23 +0100

    Slightly improve 2025.08

--- src/aoc/2025/day08.clj
+++ src/aoc/2025/day08.clj
@@ -5,55 +5,46 @@
 
 (def input (d/day-input 2025 8))
 
-(defn- pairs
-  [elements]
-  (for [i (range (count elements))
-        j (range (count elements))
-        :while (< j i)]
-    [(nth elements i) (nth elements j)]))
+(defn- pairs [xs]
+  (for [i (range (count xs))
+        j (range i)]
+    [(nth xs i) (nth xs j)]))
 
 (defn- distance [[x y z] [x' y' z']]
   (Math/sqrt (+ (Math/pow (- x' x) 2) (Math/pow (- y' y) 2) (Math/pow (- z' z) 2))))
 
-(defn part1
-  ([input] (part1 input 1000))
-  ([input n]
-   (let [shortest-pairs
-         (->> input
-              s/parse-lines
-              (map s/parse-ints)
-              pairs
-              (map (fn [[a b]] {[a b] (distance a b)}))
-              (into {})
-              (sort-by val)
-              (take n))]
-     (->> shortest-pairs
-          (reduce (fn [sets [[one two] distance]]
-                    (let [s1 (some #(if (contains? % one) % nil) sets)
-                          s2 (some #(if (contains? % two) % nil) sets)]
-                      (-> (remove
-                           #(or (= s1 %)
-                                (= s2 %)) sets)
-                          (conj (set/union (or s1 #{one}) (or s2 #{two}))))))
-                  [])
-          (map count)
-          (sort >)
-          (take 3)
-          (reduce *)))))
+(defn part1 [input]
+  (->> input
+       s/parse-lines
+       (map s/parse-ints)
+       pairs
+       (map (fn [[a b]] [(distance a b) a b]))
+       sort
+       (take 1000)
+       (reduce (fn [sets [distance one two]]
+                 (let [s1 (some #(if (contains? % one) % nil) sets)
+                       s2 (some #(if (contains? % two) % nil) sets)]
+                   (-> (remove
+                        #(or (= s1 %)
+                             (= s2 %)) sets)
+                       (conj (set/union (or s1 #{one}) (or s2 #{two}))))))
+               [])
+       (map count)
+       (sort >)
+       (take 3)
+       (reduce *)))
 
 (defn part2 [input]
   (let [boxes (->> input
                    s/parse-lines
                    (map s/parse-ints))
-        shortest-pairs
-        (->> boxes
-             pairs
-             (map (fn [[a b]] {[a b] (distance a b)}))
-             (into {})
-             (sort-by val))]
+        shortest-pairs (->> boxes
+                            pairs
+                            (map (fn [[a b]] [(distance a b) a b]))
+                            sort)]
     (loop [sets []
            remaining shortest-pairs]
-      (let [[[one two] distance] (first remaining)
+      (let [[distance one two] (first remaining)
             s1 (some #(if (contains? % one) % nil) sets)
             s2 (some #(if (contains? % two) % nil) sets)
             new-sets
