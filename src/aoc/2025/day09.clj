0f805baede22def69492f56e5b8aaa0ee1cab902
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-10 11:30:48 +0100

    Finish 2025.09

--- src/aoc/2025/day09.clj
+++ src/aoc/2025/day09.clj
@@ -1,7 +1,6 @@
 (ns aoc.2025.day09
   (:require [aoc.util.day :as d]
-            [aoc.util.string :as s]
-            ))
+            [aoc.util.string :as s]))
 
 (def input (d/day-input 2025 9))
 
@@ -14,12 +13,65 @@
   (* (inc (abs (- x x')))
      (inc (abs (- y y')))))
 
-(defn part1 [input]
-  (->> input
-       s/parse-lines
-       (mapv s/parse-ints)
+(defn- largest-square [points]
+  (->> points
        pairs
        (map (fn [[a b]] [(size a b) a b]))
        sort
        last
        first))
+
+(defn part1 [input]
+  (->> input
+       s/parse-lines
+       (mapv s/parse-ints)
+       largest-square))
+
+(defn- outliers [points]
+  (reduce
+   (fn [[outliers [x y :as prev]] [x' y' :as next]]
+     (cond
+       (< 10 (/ x x')) [(conj outliers prev) next]
+       (< 10 (/ x' x)) [(conj outliers next) next]
+       :else [outliers next]))
+   [[] (first points)]
+   (rest points)))
+
+(defn- y-limit [points x-cutoff]
+  (->> points
+       (take-while #(> (first %) x-cutoff))
+       last
+       last))
+
+(defn- max-size [anchor points]
+  (->> points
+       (map #(size % anchor))
+       (reduce max)))
+
+(defn part2
+  "For this puzzle we require some knowledge about our input. Put the
+  points in desmos' polygon() function and see what I mean.
+
+  We split our circle in two parts, north and south. For each
+  half-circle we discard the input of the other half and find the
+  biggest circle, with one caveat: the y-limit must be not too far
+  from our north and south anchors. Again, look at the polygon."
+  [input]
+  (let [points (->> input
+                    s/parse-lines
+                    (mapv s/parse-ints))
+        [outliers _] (outliers points)
+
+        north-point (apply max-key last outliers)
+        south-point (apply min-key last outliers)
+
+        nx (first north-point)
+        sx (first south-point)
+
+        ny-limit (y-limit points nx)
+        sy-limit (y-limit (rseq points) sx)
+
+        north-half (filter #(<= (last north-point) (last %) ny-limit) points)
+        south-half (filter #(>= (last south-point) (last %) sy-limit) points)]
+    (max (max-size north-point north-half)
+         (max-size south-point south-half))))
