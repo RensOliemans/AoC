0ff78c3ca2846aa2426406a3c4176e37653fb681
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-11 13:41:51 +0100

    Simplify 2025.11 more (don't invert-map)

--- src/aoc/2025/day11.clj
+++ src/aoc/2025/day11.clj
@@ -1,7 +1,6 @@
 (ns aoc.2025.day11
   (:require [aoc.util.day :as d]
             [aoc.util.string :as s]
-            [aoc.util.vec :as v]
             [clojure.string :as str]))
 
 (def input (d/day-input 2025 11))
@@ -9,18 +8,17 @@
 (defn- parse-input [input]
   (->> input
        s/parse-lines
-       (map #(str/split % #":? "))
-       (map (fn [[from & to]] {from (set to)}))
-       (into {})
-       v/invert-map-of-sets))
+       (map #(re-seq #"\w+" %))
+       (map (fn [[from & to]] [from to]))
+       (into {})))
 
 (defn- count-paths-to-goal-dac-fft
-  ([m goal cur] (count-paths-to-goal-dac-fft m goal cur false false))
-  ([m goal cur dac fft]
+  ([m cur goal] (count-paths-to-goal-dac-fft m cur goal false false))
+  ([m cur goal dac fft]
    (if (= cur goal)
      (if (and dac fft) 1 0)
      (let [neighbours (m cur)]
-       (reduce + (map #(count-paths-to-goal-dac-fft m goal %
+       (reduce + (map #(count-paths-to-goal-dac-fft m % goal
                                                     (or dac (= cur "dac"))
                                                     (or fft (= cur "fft")))
                       neighbours))))))
