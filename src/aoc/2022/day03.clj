8f5f59e943d855ff7935f9e1c574d5323eee6103
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-16 18:49:44 +0100

    Finish 2022.03

---
+++ src/aoc/2022/day03.clj
@@ -0,0 +1,36 @@
+(ns aoc.2022.day03
+  (:require [aoc.util.day :as d]
+            [aoc.util.string :as s]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+(def input (d/day-input 2022 03))
+
+(defn- split-in-two [v]
+  (let [c (count v)
+        h (quot c 2)]
+    [(subvec v 0 h) (subvec v h)]))
+
+(defn- common-items [& vecs]
+  (apply set/intersection (map set vecs)))
+
+(defn- char-to-num [c]
+  (if (Character/isUpperCase c)
+    (- (int c) 38)
+    (- (int c) 96)))
+
+(defn part1 [input]
+  (->> (s/parse-lines input)
+       (map vec)
+       (map split-in-two)
+       (mapcat (fn [[one two]] (common-items one two)))
+       (map char-to-num)
+       (reduce +)))
+
+(defn part2 [input]
+  (->> (s/parse-lines input)
+       (map vec)
+       (partition 3)
+       (mapcat (fn [[one two three]] (common-items one two three)))
+       (map char-to-num)
+       (reduce +)))
