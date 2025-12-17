9e5ef55b457f797d35e838937e1a4a1cec4a65b2
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-17 16:16:57 +0100

    Finish 2022.07

---
+++ src/aoc/2022/day07.clj
@@ -0,0 +1,55 @@
+(ns aoc.2022.day07
+  (:require [aoc.util.day :as d]
+            [aoc.util.string :as s]
+            [clojure.string :as str]))
+
+(def input (d/day-input 2022 07))
+
+(defn- directory-sizes [input]
+  (let [[pwd dir-sizes]
+        (reduce
+         (fn [[pwd dir-sizes] line]
+           (condp re-matches line
+             #"\$ ls" [pwd dir-sizes]
+             #"dir \w+" [pwd dir-sizes]
+             #"\$ cd \.\." [(pop pwd) dir-sizes]
+             #"\$ cd /" [(conj pwd "/") (assoc dir-sizes "/" 0)]
+             #"\$ cd ([\w]+)"
+             :>> (fn [[_ dir]]
+                   (let [d (str "/" dir)]
+                     [(conj pwd d) (assoc dir-sizes (apply str (conj pwd d)) 0)]))
+             #"([0-9]+) .+"
+             :>> (fn [[_ filesize-str]]
+                   (let [filesize (parse-long filesize-str)]
+                     ;; add filesize to current directory and all
+                     ;; parent dirs (in pwd)
+                     [pwd
+                      (loop [sizes dir-sizes
+                             pwd' pwd]
+                        (if (empty? pwd')
+                          sizes
+                          (recur
+                           (update sizes (apply str pwd') #(+ filesize %))
+                           (rest pwd'))))]))))
+         ['() {}]
+         (s/parse-lines input))]
+    dir-sizes))
+
+(defn part1 [input]
+  (let [sizes (directory-sizes input)]
+    (->> sizes
+         (filter #(> 100000 (last %)))
+         (map last)
+         (reduce +))))
+
+(defn part2 [input]
+  (let [sizes (directory-sizes input)
+        used-space (sizes "/")
+        unused-space (- 70000000 used-space)
+        deletion-size (- 30000000 unused-space)
+        ]
+    (->> sizes
+         (sort-by val)
+         (drop-while #(> deletion-size (val %)))
+         first
+         val)))
