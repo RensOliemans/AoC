a0e8ddd9be7a2c4bce361db8947d72339eb7c74c
Rens Oliemans <hallo@rensoliemans.nl>
2025-12-19 11:03:36 +0100

    Improve 2024.07

--- src/aoc/2024/day07.clj
+++ src/aoc/2024/day07.clj
@@ -1,34 +1,50 @@
 (ns aoc.2024.day07
   (:require [aoc.util.day :as d]
             [clojure.string :as str] 
-            [aoc.util.string :as s]
-            ;; [aoc.util.vec :as v]
-	    ))
+            [aoc.util.string :as s]))
 
 (def input (d/day-input 2024 07))
 
-(defn- is-correct? [[result & numbers] operators]
-  (let [possibilities
-        (reduce (fn [acc i]
-                  (mapcat (fn [n] (map #(% n i) operators)) acc))
-                [(first numbers)]
-                (rest numbers))]
-    (some #(= result %) possibilities)))
+(defn- operators-1 []
+  [{:name :multiplication :reverse #(/ %1 %2) :valid? #(= 0 (mod %1 %2))}
+   {:name :addition :reverse #(- %1 %2) :valid? #(> %1 %2)}])
+
+(defn- operators-2 []
+  (into (operators-1)
+        [{:name :concatenation
+          :reverse (fn [n x]
+                     (let [s (str n)]
+                       (Long/parseLong (subs s 0 (- (count s) (count (str x)))))))
+          :valid? (fn [n x]
+                    (let [sn (str n)
+                          sx (str x)]
+                      (and (> (count sn) (count sx))
+                           (str/ends-with? sn sx))))}]))
+
+(defn- is-correct?
+  "Can `target` be made with `numbers`, using `operators`?"
+  [target numbers operators]
+  (if (= 1 (count numbers))
+    (= target (first numbers))
+    (->> operators
+         (filter (fn [op] ((:valid? op) target (last numbers))))
+         (some (fn [op]
+                 (is-correct?
+                  ((:reverse op) target (last numbers))
+                  (butlast numbers)
+                  operators))))))
 
 (defn- sum-correct-equations [input operators]
   (->> (s/parse-lines input)
        (map s/parse-ints)
-       (filter #(is-correct? % operators))
+       (filter (fn [[target & numbers]] (is-correct? target numbers operators)))
        (map first)
        (reduce +)))
 
 (defn part1 [input]
-  (let [operators [#(* %1 %2)
-                   #(+ %1 %2)]]
-    (sum-correct-equations input operators)))
+  (let [ops (operators-1)]
+    (sum-correct-equations input ops)))
 
 (defn part2 [input]
-  (let [operators [#(* %1 %2)
-                   #(+ %1 %2)
-                   #(Long/parseLong (str %1 %2))]]
-    (sum-correct-equations input operators)))
+  (let [ops (operators-2)]
+    (sum-correct-equations input ops)))
